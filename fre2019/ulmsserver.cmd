0ba057d514583e51356c247ae165b732ef0047e1
Maizerunner <maizerunners@gmail.com>
2023-03-31 11:47:53 +0200

    yo what up

---
+++ fre2019/ulmsserver.cmd
@@ -0,0 +1,100 @@
+rosebot mapInit
+rosebot mapUpdate
+rosebot PFinit
+rosebot PFrun
+rosebot mapInit
+rosebot mapUpdate
+rosebot PFinit
+rosebot PFrun
+rosebot mapInit
+rosebot mapUpdate
+rosebot mapInit
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapInit
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapInit
+rosebot mapUpdate
+rosebot mapInit
+rosebot mapUpdate
+rosebot mapInit
+rosebot mapUpdate
+rosebot mapInit
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapInit
+rosebot mapUpdate
+rosebot mapPrint
+push t="3.0" cmd="rosebot mapUpdate"
+rosebot mapPrint
+rosebot mapInit
+rosebot mapPrint
+rosebot mapInit
+rosebot mapPrint
+rosebot mapInit
+rosebot mapPrint
+rosebot mapReset
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapReset
+rosebot mapPrint
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapUpdate
+rosebot mapInit
+exit
+rosebot mapReset
+rosebot mapUpdate
+rosebot mapPrint
+rosebot mapReset
+rosebot mapUpdate
+rosebot mapPrint
+mrc help
+help
+help scanset
+shelp
+shelp push
+help push
+push help
+scanset help
+help
+help q
+q help
+help
+alive help
+server help
+alive help
+help
+shelp help
+help
+quit help
+quit helprosebot slamprint
+rosebot printlaser
+quit helprosebot slamprint
+qqq
+help
+qqq
+mrc task1v4
+date
+time
+help
+scanget
+module load  module="auzoneobst.so.0"
+module load ="auzoneobst.so.0"
+zoneobst
+module load ="auzoneobst.so.0"
+zoneobst
+module load ="auzoneobst.so.0"
+zoneob
+zoneobst
