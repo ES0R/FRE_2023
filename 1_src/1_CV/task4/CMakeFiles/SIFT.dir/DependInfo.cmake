4b9036bd247652d6ef8c59c72b2d9ccdf1b89ce8
ES0R <emiloramovic@gmail.com>
2023-06-05 14:09:07 +0200

    tester

---
+++ 1_src/1_CV/task4/CMakeFiles/SIFT.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/er0s/Documents/FRE_2023/1_src/1_CV/task4/src/main.cpp" "CMakeFiles/SIFT.dir/src/main.cpp.o" "gcc" "CMakeFiles/SIFT.dir/src/main.cpp.o.d"
+  "/home/er0s/Documents/FRE_2023/1_src/1_CV/task4/src/utils.cpp" "CMakeFiles/SIFT.dir/src/utils.cpp.o" "gcc" "CMakeFiles/SIFT.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 1_src/1_CV/task4/CMakeFiles/SIFT.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SIFT.dir/src/main.cpp.o"
+  "CMakeFiles/SIFT.dir/src/main.cpp.o.d"
+  "CMakeFiles/SIFT.dir/src/utils.cpp.o"
+  "CMakeFiles/SIFT.dir/src/utils.cpp.o.d"
+  "SIFT"
+  "SIFT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SIFT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
